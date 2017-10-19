70389f90d09009778580e43e3bda5b0613236660
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-10-19 13:16:04 -0400

    shape

--- Classwork/src/shape/Triangle.java
+++ Classwork/src/shape/Triangle.java
@@ -3,12 +3,25 @@ package shape;
 public class Triangle {
 
 	private double side;
-	public Triangle(double side) {
+	private double side2;
+	private double side3;        
+	public Triangle(double side, double side2, double side3) {
 		this.side = side;
+		this.side2 = side2;
+		this.side3 = side3;
 	}
 	
 	public double area() {
-		return 3.6;
+		double p = (side + side2 + side3)/2;
+		double a = Math.sqrt(p * (p-side) * (p-side2) * (p-side3));
+		return a;
 	}
-
+	
+	public double perimeter() {
+		return side + side2 + side3;
+	}
+	
+	public String stringTo() {
+		return "Triangle Sides: " + side + ", " + side2 + ", " + side3 + "Area: " + this.area() + "Perimeter: " + this.perimeter();
+	}
 }
